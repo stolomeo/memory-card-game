8ac40540df15413a3b1d511f11c9b07ac95d4be2
stolomeo <tolomeosebastian@gmail.com>
2022-05-27 00:47:38 -0400

    create utils for getting random pokemon

--- src/components/utils/createPokemon.ts
+++ src/components/utils/createPokemon.ts
@@ -3,7 +3,7 @@ import { getPokemon } from "./getPokemon";
 export const createPokemon = async () => {
   const pokemon = [];
   for (let i = 0; i < 6; i++) {
-    pokemon.push(await getPokemon());
+    pokemon.push(await getPokemon(1, 12));
   }
   return pokemon;
 };
--- src/components/utils/getPokemon.ts
+++ src/components/utils/getPokemon.ts
@@ -1,5 +1,16 @@
-export const getPokemon = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * max) + min;
+};
+
+const getRandomUrl = (min: number, max: number) => {
+  const randomInt = getRandomInt(min, max);
+  const randomUrl = `https://pokeapi.co/api/v2/pokemon/${randomInt}`;
+  return randomUrl;
+};
+
+export const getPokemon = async (min: number, max: number) => {
+  const url = getRandomUrl(min, max);
+  const response = await fetch(url);
   const { id, name, sprites } = await response.json();
   return { id, name, sprites };
 };
